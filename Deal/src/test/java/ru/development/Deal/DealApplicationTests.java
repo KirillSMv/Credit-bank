4c02cd7827b12a7b44fd3d69e6cce8cd415b74e0
kiril <kirill551@yandex.ru>
2024-11-11 22:57:48 +0300

    initial commit

---
+++ Deal/src/test/java/ru/development/Deal/DealApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.development.Deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
