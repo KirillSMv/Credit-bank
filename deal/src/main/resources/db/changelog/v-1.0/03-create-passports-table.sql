176a9bbc1da0c8551833d00a22c6ffbd98f66933
Kirill <kirill551@yandex.ru>
2025-01-31 00:43:30 +0100

    "Внести правки согласно отчету SonarCloud"

--- deal/src/main/resources/db/changelog/v-1.0/03-create-passports-table.sql
+++ deal/src/main/resources/db/changelog/v-1.0/03-create-passports-table.sql
@@ -1,28 +1,11 @@
-create table if not exists passports
-(
-    passport_id_uuid
-    uuid
-    unique
-    NOT
-    NULL,
-    issue_branch
-    varchar
-(
-    255
-),
-    issue_date date,
-    number varchar
-(
-    255
-) NOT NULL,
-    series varchar
-(
-    255
-) NOT NULL,
-    primary key
-(
-    passport_id_uuid
-)
+create table if not exists  passports (
+        passport_id_uuid uuid unique NOT NULL,
+        issue_branch varchar(255),
+        issue_date date,
+        number varchar(255) NOT NULL,
+        series varchar(255) NOT NULL,
+        primary key (passport_id_uuid)
     )
-    GO
+
+GO
 
